077417379e6c655e20e6dd5ea817d20937d5c69d
dev_monk <55343937+thedevyansh@users.noreply.github.com>
2020-11-21 14:55:09 +0530

    Used trained model to detect and count number of vehices

--- main1.py
+++ main1.py
@@ -1,66 +1,41 @@
-import re
-import os
-import cv2
-import numpy as np
-from os.path import isfile, join
-import matplotlib.pyplot as plt
-
-
-# empty list to store the frames
-col_images = []
-
-vidObj = cv2.VideoCapture("assets/footage.mp4") 
-
-# checks whether frames were extracted 
-success = 1
+import cv2 
   
-while success:
+cap = cv2.VideoCapture('footage2.mp4') 
 
-    success, image = vidObj.read() 
-    col_images.append(image)
+# Trained XML classifiers describes some features of some object we want to detect 
+car_cascade = cv2.CascadeClassifier('cars.xml') 
+  
+while(cap.isOpened()): 
+    ret, frames = cap.read() 
+      
+    if ret == True:
+        count = 0
+        gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY) 
+  
+        # Detects cars of different sizes in the input image 
+        cars = car_cascade.detectMultiScale(gray, 1.1, 1) 
+      
+        for (x,y,w,h) in cars: 
+            if w*h > 2500:
 
+                cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
+                cv2.circle(frames, (x+w//2, y+h//2), 1, (0,255,255), 2)
+                count = count + 1
 
-# kernel for image dilation
-kernel = np.ones((4, 4), np.uint8)
+        if count < 5:
+            print("Less...")
+        elif count > 5 and count < 20:
+            print("Moderate...")
+        else:
+            print("Heavy...")
+  
+        cv2.imshow('video', frames) 
+      
+        if cv2.waitKey(15) == 27: 
+            break
 
-frame_array = []
+    else:
+        break
 
-for i in range(len(col_images)-1):
-
-    # frame differencing
-    grayA = cv2.cvtColor(col_images[i], cv2.COLOR_BGR2GRAY)
-    grayB = cv2.cvtColor(col_images[i+1], cv2.COLOR_BGR2GRAY)
-    diff_image = cv2.absdiff(grayB, grayA)
-
-    # image thresholding
-    ret, thresh = cv2.threshold(diff_image, 70, 255, cv2.THRESH_BINARY)
-
-    # image dilation
-    dilated = cv2.dilate(thresh, kernel, iterations=1)
-
-    # find contours
-    contours, hierarchy = cv2.findContours(
-        dilated.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-    # add contours to original frames
-    dmy = col_images[i].copy()
-    cv2.drawContours(dmy, contours, -1, (127, 200, 0), 1)
-
-    height, width, layers = dmy.shape
-    size = (width,height)
-
-    frame_array.append(dmy)
-
-# specify video name
-pathOut = 'vehicle_detection.mp4'
-
-# specify frames per second
-fps = 14.0
-
-out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
-
-for i in range(len(frame_array)):
-     out.write(frame_array[i])
-
-out.release()
-
+cap.release()
+cv2.destroyAllWindows()
